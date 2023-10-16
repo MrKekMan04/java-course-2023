85fbfd8181617117de64a647c3f84d409c1e5880
Vitaliy <vitaliy-efimov111@mail.ru>
2023-10-16 23:10:00 +0500

    Refactor `Task4Test`

--- src/test/java/edu/hw2/task4/Task4Test.java
+++ src/test/java/edu/hw2/task4/Task4Test.java
@@ -7,11 +7,9 @@ public class Task4Test {
     @Test
     public void callingInfoTest() {
         var callingInfos = Task4.callingInfo();
+        var callingMethod = new CallingInfo(Task4.class.getName(), Task4.class.getMethods()[0].getName());
 
         assertTrue(callingInfos.stream()
-            .anyMatch(info -> info.equals(new CallingInfo(
-                Task4.class.getName(),
-                Task4.class.getMethods()[0].getName()
-            ))));
+            .anyMatch(info -> info.equals(callingMethod)));
     }
 }
