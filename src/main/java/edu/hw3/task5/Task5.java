c6429073b3d0a16bc3749937f56b0e0c8f9ab70b
Vitaliy <vitaliy-efimov111@mail.ru>
2023-11-06 19:25:05 +0500

    Decompose `parseContacts` and extract intermediate methods

--- src/main/java/edu/hw3/task5/Task5.java
+++ src/main/java/edu/hw3/task5/Task5.java
@@ -1,47 +1,47 @@
 package edu.hw3.task5;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
+import org.jetbrains.annotations.NotNull;
 
 public final class Task5 {
     private Task5() {
     }
 
     public static Collection<Person> parseContacts(Collection<String> contacts, String sortBy) {
-        boolean sortByAscending = switch (sortBy) {
-            case "ASC" -> true;
-            case "DESC" -> false;
-            default -> throw new IllegalArgumentException("Parameter `sortBy` must be only `ASC` or `DESC`!");
-        };
+        Comparator<Person> sortOrder = getSortOrder(sortBy);
 
-        if (contacts == null || contacts.isEmpty()) {
-            return List.of();
+        return contacts == null || contacts.isEmpty() ? List.of() : contacts.stream()
+            .map(Task5::getPersonFromContact)
+            .sorted(sortOrder)
+            .toList();
+    }
+
+    @NotNull
+    private static Person getPersonFromContact(String contact) {
+        if (contact == null) {
+            throw new NullPointerException("Person name can't be null!");
         }
 
-        ArrayList<Person> result = contacts.stream().map(contact -> {
-                if (contact == null) {
-                    throw new NullPointerException("Person name can't be null!");
-                }
+        if (contact.contains(" ")) {
+            String[] personData = contact.split(" ");
 
-                if (contact.contains(" ")) {
-                    String[] personData = contact.split(" ");
+            if (personData.length != 2) {
+                throw new IllegalArgumentException("Person data length must be 2!");
+            }
 
-                    if (personData.length != 2) {
-                        throw new IllegalArgumentException("Person data length must be 2!");
-                    }
+            return new Person(personData[0], personData[1]);
+        }
 
-                    return new Person(personData[0], personData[1]);
-                }
+        return new Person(contact, "");
+    }
 
-                return new Person(null, contact);
-            })
-            .collect(Collectors.toCollection(ArrayList::new));
-
-        return result.stream()
-            .sorted(sortByAscending ? Comparator.naturalOrder() : Comparator.reverseOrder())
-            .collect(Collectors.toList());
+    private static Comparator<Person> getSortOrder(String sortBy) {
+        return switch (sortBy) {
+            case "ASC" -> Comparator.naturalOrder();
+            case "DESC" -> Comparator.reverseOrder();
+            default -> throw new IllegalArgumentException("Parameter `sortBy` must be only `ASC` or `DESC`!");
+        };
     }
 }
