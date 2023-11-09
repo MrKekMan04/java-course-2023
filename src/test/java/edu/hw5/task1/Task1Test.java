3e258f966992683ab2febcb57da750b8acdab54e
Vitaliy <vitaliy-efimov111@mail.ru>
2023-11-09 19:48:44 +0500

    Add tests for `Task 1`

---
+++ src/test/java/edu/hw5/task1/Task1Test.java
@@ -0,0 +1,36 @@
+package edu.hw5.task1;
+
+import org.junit.jupiter.api.Test;
+import java.time.Duration;
+import static edu.hw5.task1.Task1.getSessionDuration;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class Task1Test {
+    @Test
+    public void assertThatGetSessionDurationReturnedRightAnswer() {
+        final Duration expected = Duration.ofHours(3).plus(Duration.ofMinutes(40));
+
+        final Duration actual = getSessionDuration(new String[] {
+            "2022-03-12, 20:20 - 2022-03-12, 23:50",
+            "2022-04-01, 21:30 - 2022-04-02, 01:20"
+        });
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void assertThatEmptySessionsReturnedZeroDuration() {
+        final Duration expected = Duration.ZERO;
+
+        assertEquals(expected, getSessionDuration(new String[] {}));
+    }
+
+    @Test
+    public void assertThatIncorrectInputReturnedNull() {
+        assertNull(getSessionDuration(new String[] {
+            "2022-03-12, 20:20-2022-03-12, 23:50"
+        }));
+
+        assertNull(getSessionDuration(null));
+    }
+}
