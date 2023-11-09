db53b85a48bc9d9d7f83950185cae106f1d3829e
Vitaliy <vitaliy-efimov111@mail.ru>
2023-11-09 19:52:47 +0500

    Add tests for `Task 4`

---
+++ src/test/java/edu/hw5/task4/Task4Test.java
@@ -0,0 +1,23 @@
+package edu.hw5.task4;
+
+import org.junit.jupiter.api.Test;
+import static edu.hw5.task4.Task4.isPasswordValid;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class Task4Test {
+    @Test
+    public void assertThatIsPasswordValidReturnedRightAnswer() {
+        final String containsOnlyOneSpecialSymbol = "some@password";
+        final String containsManySpecialSymbols = "AnY~O!he^&";
+        final String notContainsSpecialSymbols = "password";
+
+        assertTrue(isPasswordValid(containsOnlyOneSpecialSymbol));
+        assertFalse(isPasswordValid(containsManySpecialSymbols));
+        assertFalse(isPasswordValid(notContainsSpecialSymbols));
+    }
+
+    @Test
+    public void assertThatNullInputReturnedFalse() {
+        assertFalse(isPasswordValid(null));
+    }
+}
