f05b45193c90bf32038a807f16a2417933ec38eb
Vitaliy <vitaliy-efimov111@mail.ru>
2023-11-09 19:48:17 +0500

    Complete `Task 1`

---
+++ src/main/java/edu/hw5/task1/Task1.java
@@ -0,0 +1,29 @@
+package edu.hw5.task1;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public final class Task1 {
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd, HH:mm");
+
+    private Task1() {
+    }
+
+    public static Duration getSessionDuration(String[] sessions) {
+        try {
+            Duration duration = Duration.ZERO;
+
+            for (String session : sessions) {
+                String[] sessionTime = session.split(" - ");
+                LocalDateTime start = DATE_TIME_FORMATTER.parse(sessionTime[0], LocalDateTime::from);
+                LocalDateTime end = DATE_TIME_FORMATTER.parse(sessionTime[1], LocalDateTime::from);
+                duration = duration.plus(Duration.between(start, end));
+            }
+
+            return sessions.length > 0 ? duration.dividedBy(sessions.length) : duration;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+}
