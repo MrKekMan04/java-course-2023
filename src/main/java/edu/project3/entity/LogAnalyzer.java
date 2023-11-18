b55addb23b0a71b2c82614c813bfd8a851c33ad6
Vitaliy <vitaliy-efimov111@mail.ru>
2023-11-18 19:34:24 +0500

    Add `LogAnalyzer`

---
+++ src/main/java/edu/project3/entity/LogAnalyzer.java
@@ -0,0 +1,98 @@
+package edu.project3.entity;
+
+import edu.project3.entity.chain.AbstractFinder;
+import edu.project3.entity.chain.FileFinder;
+import edu.project3.entity.chain.FileInIntermediateDirectoriesFinder;
+import edu.project3.entity.chain.FilesInDirectoryFinder;
+import edu.project3.entity.chain.UrlFinder;
+import edu.project3.repository.LogRepository;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Stream;
+
+public class LogAnalyzer {
+    private static final String PATH_KEY = "--path";
+    private final Map<String, String> parameters;
+    private final List<String> parsedLogs;
+    private final LogRepository repository;
+    private final AbstractFinder finder;
+
+    public LogAnalyzer(Map<String, String> parameters) {
+        if (parameters.get(PATH_KEY) == null) {
+            throw new NullPointerException("Required start parameter `" + PATH_KEY + "` is not defined!");
+        }
+
+        this.parameters = parameters;
+        this.parsedLogs = new ArrayList<>();
+        this.repository = new LogRepository();
+        this.finder = buildChain();
+    }
+
+    public void analyze() {
+        String path = parameters.get(PATH_KEY);
+
+        for (Stream<String> log : finder.getLogsLines(path)) {
+            analyzeLog(log, path);
+        }
+    }
+
+    public LogRepository getRepository() {
+        return repository;
+    }
+
+    public List<String> getParsedLogs() {
+        return parsedLogs;
+    }
+
+    private AbstractFinder buildChain() {
+        AbstractFinder lastFinder = null;
+        List<Class<? extends AbstractFinder>> classes = List.of(
+            UrlFinder.class, FilesInDirectoryFinder.class, FileInIntermediateDirectoriesFinder.class, FileFinder.class
+        );
+
+        for (Class<? extends AbstractFinder> aClass : classes) {
+            try {
+                lastFinder = aClass.getConstructor(AbstractFinder.class).newInstance(lastFinder);
+            } catch (ReflectiveOperationException ignored) {
+            }
+        }
+
+        return lastFinder;
+    }
+
+    private void analyzeLog(Stream<String> lines, String path) {
+        LocalDate parseFrom = parseTime(parameters.get("--from"));
+        LocalDate parseTo = parseTime(parameters.get("--to"));
+
+        lines.map(this::tryParseLogLine)
+            .filter(Objects::nonNull)
+            .filter(logRecord -> isBetween(logRecord, parseFrom, parseTo))
+            .forEach(repository::add);
+
+        parsedLogs.add(path);
+    }
+
+    private LocalDate parseTime(String time) {
+        return time == null ? null : LocalDate.parse(time, DateTimeFormatter.ISO_DATE);
+    }
+
+    private LogRecord tryParseLogLine(String line) {
+        try {
+            return new LogRecord(line);
+        } catch (Exception ignored) {
+            return null;
+        }
+    }
+
+    private boolean isBetween(LogRecord logRecord, LocalDate from, LocalDate to) {
+        LocalDate recordDate = logRecord.getDateTime().toLocalDate();
+
+        return (to == null || Period.between(to, recordDate).isNegative())
+            && (from == null || Period.between(recordDate, from).isNegative());
+    }
+}
