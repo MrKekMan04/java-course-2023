0a4ac6a8b8304140bae08b09f43c8665c171df5f
Vitaliy <vitaliy-efimov111@mail.ru>
2023-11-30 15:26:13 +0500

    Override queue factory

--- src/main/java/edu/project2/ai/solver/AStarPathfinder.java
+++ src/main/java/edu/project2/ai/solver/AStarPathfinder.java
@@ -1,12 +1,21 @@
 package edu.project2.ai.solver;
 
 import edu.project2.entity.Coordinate;
+import edu.project2.entity.CoordinateNode;
 import edu.project2.entity.WeightedCoordinate;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.function.Supplier;
 
 public class AStarPathfinder extends BreadthFirstSearchPathfinder {
     @Override
     protected Coordinate createNextCoordinate(Coordinate next, Coordinate end) {
         return new WeightedCoordinate(next.getRow(), next.getCol(), calculateDistance(next, end));
+    }
+
+    @Override
+    protected Supplier<? extends Queue<CoordinateNode>> queueFactory() {
+        return PriorityQueue::new;
     }
 
     private static double calculateDistance(Coordinate from, Coordinate to) {
