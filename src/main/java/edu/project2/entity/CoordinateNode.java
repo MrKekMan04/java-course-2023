ef77c21e6e5331febc6e1a0f13fd650d8a7612c6
Vitaliy <vitaliy-efimov111@mail.ru>
2023-11-30 17:06:17 +0500

    Remove comparable implementation and make generic

--- src/main/java/edu/project2/entity/CoordinateNode.java
+++ src/main/java/edu/project2/entity/CoordinateNode.java
@@ -1,14 +1,7 @@
 package edu.project2.entity;
 
-import org.jetbrains.annotations.NotNull;
-
-public record CoordinateNode(CoordinateNode child, Coordinate coordinate) implements Comparable<CoordinateNode> {
-    public CoordinateNode(Coordinate coordinate) {
+public record CoordinateNode<T extends Coordinate>(CoordinateNode<T> child, T coordinate) {
+    public CoordinateNode(T coordinate) {
         this(null, coordinate);
-    }
-
-    @Override
-    public int compareTo(@NotNull CoordinateNode o) {
-        return coordinate.compareTo(o.coordinate);
     }
 }
