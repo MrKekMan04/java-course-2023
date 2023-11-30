e4a89dc2e90443bf597b95de2e66d47deac16360
Vitaliy <vitaliy-efimov111@mail.ru>
2023-11-30 16:33:30 +0500

    Add tests for `RecursiveDepthFirstGenerator`

--- src/test/java/edu/project2/ai/generator/MazeGeneratorTest.java
+++ src/test/java/edu/project2/ai/generator/MazeGeneratorTest.java
@@ -1,6 +1,7 @@
 package edu.project2.ai.generator;
 
 import edu.project2.entity.Cell;
+import edu.project2.entity.Maze;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -11,39 +12,52 @@ import static org.junit.jupiter.api.Assertions.*;
 public class MazeGeneratorTest {
     private static final int LOOP_TEST_ITERATIONS = 1_000;
     private static Random random;
-    private static RandomizedPrimGenerator generator;
+    private static MazeGenerator randomizedPrimGenerator;
+    private static MazeGenerator recursiveDepthFirstGenerator;
 
     @BeforeAll
     public static void setUp() {
         random = new Random();
-        generator = new RandomizedPrimGenerator();
+        randomizedPrimGenerator = new RandomizedPrimGenerator();
+        recursiveDepthFirstGenerator = new RecursiveDepthFirstGenerator();
     }
 
     @Test
     public void assertThatWithPrimeGeneratorPassagesGeneratesMoreThen30PercentTest() {
         IntStream.range(0, LOOP_TEST_ITERATIONS)
-            .mapToObj(i -> generator.generate(random.nextInt(10, 100), random.nextInt(10, 100)))
-            .forEach(maze -> {
-                int walls = 0;
-                int passages = 0;
-                Cell[][] grid = maze.grid();
+            .mapToObj(i -> randomizedPrimGenerator.generate(random.nextInt(10, 100), random.nextInt(10, 100)))
+            .forEach(maze -> assertTrue(getMazePassagesRate(maze) > 0.3));
+    }
 
-                for (Cell[] row : grid) {
-                    for (Cell cell : row) {
-                        switch (cell.type()) {
-                            case WALL -> walls++;
-                            case PASSAGE -> passages++;
-                        }
-                    }
-                }
-
-                assertTrue(passages / ((double) passages + walls) > 0.3);
-            });
+    @Test
+    public void assertThatWithRecursiveDepthFirstPassagesGeneratesMoreThen30PercentTest() {
+        IntStream.range(0, LOOP_TEST_ITERATIONS)
+            .mapToObj(i -> recursiveDepthFirstGenerator.generate(random.nextInt(10, 100), random.nextInt(10, 100)))
+            .forEach(maze -> assertTrue(getMazePassagesRate(maze) > 0.3));
     }
 
     @Test
     public void assertThatIncorrectInputReturnedNullTest() {
-        assertNull(generator.generate(-1, 20));
-        assertNull(generator.generate(1, 0));
+        assertNull(randomizedPrimGenerator.generate(-1, 20));
+        assertNull(randomizedPrimGenerator.generate(1, 0));
+        assertNull(recursiveDepthFirstGenerator.generate(-1, 20));
+        assertNull(recursiveDepthFirstGenerator.generate(1, 0));
+    }
+
+    private double getMazePassagesRate(Maze maze) {
+        int walls = 0;
+        int passages = 0;
+        Cell[][] grid = maze.grid();
+
+        for (Cell[] row : grid) {
+            for (Cell cell : row) {
+                switch (cell.type()) {
+                    case WALL -> walls++;
+                    case PASSAGE -> passages++;
+                }
+            }
+        }
+
+        return passages / ((double) passages + walls);
     }
 }
