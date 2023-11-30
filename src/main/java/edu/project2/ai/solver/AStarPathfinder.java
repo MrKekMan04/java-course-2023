656e19fffeae3b20f3ebf96233de8026e1081e3a
Vitaliy <vitaliy-efimov111@mail.ru>
2023-11-30 17:07:20 +0500

    Add generic types

--- src/main/java/edu/project2/ai/solver/AStarPathfinder.java
+++ src/main/java/edu/project2/ai/solver/AStarPathfinder.java
@@ -3,19 +3,20 @@ package edu.project2.ai.solver;
 import edu.project2.entity.Coordinate;
 import edu.project2.entity.CoordinateNode;
 import edu.project2.entity.WeightedCoordinate;
+import java.util.Comparator;
 import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.function.Supplier;
 
-public class AStarPathfinder extends BreadthFirstSearchPathfinder {
+public class AStarPathfinder extends BasePathfinder<WeightedCoordinate> {
     @Override
-    protected Coordinate createNextCoordinate(Coordinate next, Coordinate end) {
+    protected WeightedCoordinate createNextCoordinate(Coordinate next, Coordinate end) {
         return new WeightedCoordinate(next.getRow(), next.getCol(), calculateDistance(next, end));
     }
 
     @Override
-    protected Supplier<? extends Queue<CoordinateNode>> queueFactory() {
-        return PriorityQueue::new;
+    protected Supplier<Queue<CoordinateNode<WeightedCoordinate>>> queueFactory() {
+        return () -> new PriorityQueue<>(Comparator.comparingDouble(value -> value.coordinate().getWeight()));
     }
 
     private static double calculateDistance(Coordinate from, Coordinate to) {
